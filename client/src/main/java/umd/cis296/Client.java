c653398625f6dd9dcafaf877ab4e3f7aa34358a1
Michael-V-21 <mdvarva@umich.edu>
2025-12-05 15:26:55 -0500

    Successful Client/Server Connection

--- client/src/main/java/umd/cis296/Client.java
+++ client/src/main/java/umd/cis296/Client.java
@@ -1,10 +1,21 @@
 package umd.cis296;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client
 {
 
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 5050;
 
+    public static void main(String[] args) {
+        System.out.println("Starting client...");
+
+        try (Socket socket = new Socket(HOST, PORT)) {
+            System.out.println("Connected to server!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- server/src/main/java/umd/cis296/Server.java
+++ server/src/main/java/umd/cis296/Server.java
@@ -1,10 +1,28 @@
 package umd.cis296;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server
 {
-  public static void main(String[] args) {
 
-    System.out.println("Hello, Server!");
+    public static final int PORT = 5050;
 
-  }
+    public static void main(String[] args) {
+        System.out.println("Starting server...");
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server started on port " + PORT);
+            System.out.println("Waiting for clients...");
+
+            while (true) {
+                Socket client = serverSocket.accept();
+                System.out.println("Client connected: " + client.getInetAddress());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
