e4551d35eeb88234136fe7e2444e1dd164b99391
AhmadMajed21 <majedahmad03@hotmail.com>
2025-12-09 16:36:37 -0500

    page navigation

--- client/src/main/java/umd/cis296/ClientGUI.java
+++ client/src/main/java/umd/cis296/ClientGUI.java
@@ -1,9 +1,15 @@
 package umd.cis296;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ClientGUI extends Application {
 
@@ -18,6 +24,22 @@ public class ClientGUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private Stage getStage(ActionEvent event) {
+        return (Stage) ((Node) event.getSource()).getScene().getWindow();
+    }
+
+    private void switchScene(ActionEvent event, String fxmlFile) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Stage stage = getStage(event);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @FXML
+    private void switchToServer(ActionEvent event) throws IOException {
+        switchScene(event, "/Server_Layout.fxml");
     }
 }
 
